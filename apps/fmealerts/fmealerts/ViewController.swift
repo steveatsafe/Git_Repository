f6e0d054a43256cda47835f9d5d27e35673e3fec
Steve MacCabe <steve.maccabe@safe.com>
2016-03-29 14:26:06 -0700

    Copy from Source

---
+++ apps/fmealerts/fmealerts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fmealerts
+//
+//  Created by Angus Lau on 2015-09-19.
+//  Copyright © 2015 Safe Software Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
